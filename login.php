7db499498ff0e6f3fa3c87c4eaa6e60825b1e071
kalbaj <kalbaj@miamioh.edu>
2023-05-01 17:01:06 -0400

    finished login.php

--- login.php
+++ login.php
@@ -9,11 +9,14 @@ include "sql.inc";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	// Step 3 (Process logout POST variable and set session)
-
+	if ($_POST == 'logout') {
+		$_SESSION['loggedin'] = false;
+	}
 
 	// Step 1
 	// Get the username and pasword from the $_POST variables
-
+	$username = $_POST['username'];
+	$password = $_POST['password'];
 
 	// Prepare and execute the SQL query to validate the login credentials
 	try {
@@ -21,15 +24,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		// Set session variable
 		if (count($DATA) > 0) {
 			// user and password worked - set session variables
+			$_SESSION['loggedin'] = true;
 		}
 		else {
 			// No data retrieved - username/password not found
 			// Set session variable to not logged in
+			$_SESSION['loggedin'] = false;
 		}
 	}
 	catch  (Exception $e) {
 		// Database Error
 		// Set session variable to not logged in
+		$_SESSION['loggedin'] = false;
 	}
 
 }
@@ -44,11 +50,28 @@ Print "
 <body>";
 if (!isset($_SESSION['loggedin']) || !$_SESSION['loggedin']) {
     //  Write the login form here  (2.a.i)
-    print ("");
+    print ("
+	<h1>Login</h1>
+	<form action='login.php' method='post'>
+		<label for='username'>Username:</label>
+		<input type='text' name='username' id='username' required>
+		<br>
+		<br>
+		<label for='password'>Password:</label>
+		<input type='password' name='password' id='password' required>
+		<br>
+		<br>
+		<input type='submit' value='Login'>
+	</form>
+	");
 }
 else {
     //  Write the logged in secure data here (add a log out function) (2.b.i)
-    print ("");
+    print ("
+	<p>In the 2010 game \"Halo: Reach\" Jorge is the first member of noble team to die
+	<form action='login.php' method='post'>
+		<input type='submit' name='logout' value='Logout'>
+	</form>");
 }
 ?>
 </body>
