00cc696b576d4d96b69c8fc44d03ebdba544f275
kalbaj <kalbaj@miamioh.edu>
2023-05-06 15:44:01 -0400

    added icon

--- exchanges.js
+++ exchanges.js
@@ -1,4 +1,4 @@
-var URL="https://api.polygon.io/v3/reference/";
+var URL="https://api.polygon.io/";
 var key="oVhmIj_vHLKvXKNcn3LNn95ImprN3LgO";
 var ticker=null;
 var exchange=null;
@@ -8,7 +8,7 @@ getNews();
 
 function getExchange() {
     a=$.ajax({
-        url: URL + 'exchanges?asset_class=stocks&apiKey=' + key,
+        url: URL + 'v3/reference/exchanges?asset_class=stocks&apiKey=' + key,
         method: "GET"
     }).done(function(data) {
         $("#exchanges").html("");
@@ -19,7 +19,6 @@ function getExchange() {
         $("#exchanges").on('change',function() {
             var val = $(this).val();
             getTicker(val);
-            console.log(val);
         });
     })
 
@@ -27,7 +26,7 @@ function getExchange() {
 
 function getTicker($exchange) {
     a=$.ajax({
-        url: URL + "tickers?exchange=" + $exchange + "&active=true&apiKey=" + key,
+        url: URL + "v3/reference/tickers?exchange=" + $exchange + "&active=true&apiKey=" + key,
         method: "GET"
     }).done(function(data) {
         $("#Stocks").html("<select name=\"Stock\" id=\"stock\"></select>");
@@ -36,18 +35,38 @@ function getTicker($exchange) {
         for (i=0;i<len;i++) {
             $("#stock").append("<option value=\"" + data.results[i].ticker + "\">" + data.results[i].name + "</option>");
         }
-        $("#Stocks").append("<input type=\"button\" value=\"Details\" id=\"Details\"><input type=\"button\" value=\"News\">");
+        $("#Stocks").append("<input type=\"button\" value=\"Details\" id=\"Details\"><input type=\"button\" value=\"News\" id=\"News\">");
         $("#Details").click( function() {
             getDetails($("#stock").val());
+        });
+        $("#News").click(function() {
+            var val = $("#stock").val();
+            getNews(val);
         });
     })
 }
 
 function getDetails($ticker) {
     a=$.ajax({
-        url: URL + "tickers/" + $ticker + "?apiKey=" + key,
+        url: URL + "v3/reference/tickers/" + $ticker + "?apiKey=" + key,
+        method: "GET",
+        success: function(data) {
+
+        }
+    }).done(function(data) {
+        console.log(data);
+    })
+}
+
+function getNews($ticker) {
+    a=$.ajax({
+        url: URL + "v2/reference/news?ticker=" + $ticker + "&apiKey=" + key,
         method: "GET"
     }).done(function(data) {
         console.log(data);
+    })
+
+    $.ajax({
+        url: "http://172.17.12.35/cse383_final/final.class.php"
     })
 }
