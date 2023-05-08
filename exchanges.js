948c85b65e6510b73693d1c042770699cfebe113
kalbaj <kalbaj@miamioh.edu>
2023-05-07 22:13:23 -0400

    I GOT IMAGES TO WORK THIS IS SO EPIC

--- exchanges.js
+++ exchanges.js
@@ -58,8 +58,10 @@ function getDetails($ticker) {
         method: "GET",
     }).done(function(data) {
         console.log(data);
+        $("#info").html("")
+        $("#info").append("<img src=\"" + data.results.branding.logo_url + "?apiKey=" + key + "\" alt='" + data.results.ticker + "'>");
         b=$.ajax({
-            url: php + "setStock&stockTicker=" + $ticker + "&queryType=detail&jsonData=" + data,
+            url: php + "setStock&stockTicker=" + $ticker + "&queryType=detail&jsonData=" + data.results,
             method: "POST"
         });
     })
@@ -72,7 +74,7 @@ function getNews($ticker) {
     }).done(function(data) {
         console.log(data);
         $.ajax({
-            url: php + "setStock&stockTicker=" + $ticker + "&queryType=news&jsonData=" + data,
+            url: php + "setStock&stockTicker=" + $ticker + "&queryType=news&jsonData=" + data.results,
             method: "POST"
         });
     })
